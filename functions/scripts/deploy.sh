823740ff631613a0c03145527bf344b736fd62a4
Gal Rubin <galrubin15@gmail.com>
2025-11-01 21:29:30 +0200

    deploy our azure function + configure it

---
+++ functions/scripts/deploy.sh
@@ -0,0 +1,1 @@
+func azure functionapp publish tastematcher-dev-func --typescript
--- scripts/dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -e
-# start backend and frontend in parallel; keep logs visible
-# uses pnpm workspace filter
-pnpm --filter backend dev &
-BACKEND_PID=$!
-pnpm --filter frontend dev &
-FRONTEND_PID=$!
-wait $BACKEND_PID $FRONTEND_PID
